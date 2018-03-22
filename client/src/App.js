8282a3aabc5a1e22684fa3b0bd6be6b2497d6043
JD Pagano <jonathanpagano@gmail.com>
2018-03-22 12:24:06 -0400

    Fix App component styling
    Delete some of the custom styling, in favor of using design system
    classes
    Remove reference to Design System

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="ds-base--inverse ds-u-padding--3 ds-u-display--flex ds-u-justify-content--between ds-u-align-items--center">
           <h1 className="ds-h3 ds-u-margin-bottom--0">
-            <a href="/" title="Home" className="c-header__title">
+            <a href="/" title="Home" className="c-header__title ds-u-text-decoration--none ds-u-color--base-inverse">
               Maryland Benefits
             </a>
           </h1>
@@ -24,10 +24,9 @@ class App extends Component {
             <a href="/" title="Home">Home</a>
             <a href="/about" title="About" className="ds-u-padding--1">About</a>
           </div>
-          <div>
-          Brought to you by Nava PBC and the <a href="https://design.cms.gov/" target="_none">
-            Design System
-            </a>
+
+          <div className="ds-u-padding-y--3">
+            Brought to you by <a href="https://www.navapbc.com">Nava PBC</a>
           </div>
         </footer>
       </div>
