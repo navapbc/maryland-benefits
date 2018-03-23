d7a7db5a6ef9ba4c91ea71dc1ed37b765a9d7b17
JD Pagano <jonathanpagano@gmail.com>
2018-03-23 13:54:39 -0400

    Move Router into App.js
    Move the Router into App.js so that the header & footer can properly use
    the Link component to route without causing page refreshes

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,62 @@
 import React, { Component } from 'react';
-import Router from './Routes';
+import { Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Routes from './routes';
 
+// CSS
 import './App.css';
+
+// Page Components
+import Home from './components/Home';
+import About from './components/About';
+import Test from './components/Test';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="ds-base--inverse ds-u-padding--3 ds-u-display--flex ds-u-justify-content--between ds-u-align-items--center">
-          <h1 className="ds-h3 ds-u-margin-bottom--0">
-            <a href="/" title="Home" className="c-header__title ds-u-text-decoration--none ds-u-color--base-inverse">
-              Maryland Benefits
-            </a>
-          </h1>
-        </header>
+      <Router>
+        <div className="App">
+          <header className="ds-base--inverse ds-u-padding--3 ds-u-display--flex ds-u-justify-content--between ds-u-align-items--center">
+            <h1 className="ds-h3 ds-u-margin-bottom--0">
+              <Link to="/" title="Home" className="c-header__title ds-u-text-decoration--none ds-u-color--base-inverse">
+                Maryland Benefits
+              </Link>
+            </h1>
+          </header>
 
-        <div className="ds-l-row ds-u-margin--0 c-body--wrapper">
-          {Router}
+          <div className="ds-l-row ds-u-margin--0 c-body--wrapper">
+            <div>
+              <Route
+                exact
+                path={Routes.home}
+                component={Home}
+              />
+              <Route
+                exact
+                path={Routes.about}
+                component={About}
+              />
+              <Route
+                exact
+                path={Routes.test}
+                component={Test}
+              />
+            </div>
+          </div>
+
+          <footer className="ds-u-padding--3">
+            <div>
+              <Link to="/" title="Home">Home</Link>
+              <Link to="/about" title="About" className="ds-u-padding--1">About</Link>
+            </div>
+
+            <div className="ds-u-padding-y--3">
+              Brought to you by <a href="https://www.navapbc.com">Nava PBC</a>
+            </div>
+          </footer>
         </div>
-
-        <footer className="ds-u-padding--3">
-          <div>
-            <a href="/" title="Home">Home</a>
-            <a href="/about" title="About" className="ds-u-padding--1">About</a>
-          </div>
-
-          <div className="ds-u-padding-y--3">
-            Brought to you by <a href="https://www.navapbc.com">Nava PBC</a>
-          </div>
-        </footer>
-      </div>
+      </Router>
     );
   }
 }
---
+++ client/src/routes.js
@@ -0,0 +1,7 @@
+export const basePath = '/';
+
+export default {
+  home: `${basePath}`,
+  about: `${basePath}about`,
+  test: `${basePath}test`
+};
